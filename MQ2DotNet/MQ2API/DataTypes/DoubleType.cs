9944dbfe1c82a78a5d0423a22ca15acba402f692
Anthony Spruyt <aspruyt@hotmail.co.uk>
2023-06-25 22:31:11 +1000

    Update DoubleType to latest spec

--- MQ2DotNet/MQ2API/DataTypes/DoubleType.cs
+++ MQ2DotNet/MQ2API/DataTypes/DoubleType.cs
@@ -3,7 +3,8 @@
 namespace MQ2DotNet.MQ2API.DataTypes
 {
     /// <summary>
-    /// MQ2 type for a double precision float
+    /// MQ2 type for a double precision float.
+    /// Last Verified: 2023-06-25
     /// </summary>
     [PublicAPI]
     [MQ2Type("double")]
@@ -12,6 +13,36 @@ namespace MQ2DotNet.MQ2API.DataTypes
         internal DoubleType(MQ2TypeFactory mq2TypeFactory, MQ2TypeVar typeVar) : base(mq2TypeFactory, typeVar)
         {
         }
+
+        /// <summary>
+        /// TODO: new member
+        /// </summary>
+        public string Deci => GetMember<StringType>("Deci");
+
+        /// <summary>
+        /// TODO: new member
+        /// </summary>
+        public string Centi => GetMember<StringType>("Centi");
+
+        /// <summary>
+        /// TODO: new member
+        /// </summary>
+        public string Milli => GetMember<StringType>("Milli");
+
+        /// <summary>
+        /// TODO: new member
+        /// </summary>
+        public int? Int => GetMember<IntType>("Int");
+
+        /// <summary>
+        /// TODO: new member
+        /// </summary>
+        public string Precision => GetMember<StringType>("Precision");
+
+        /// <summary>
+        /// TODO: new member
+        /// </summary>
+        public string Prettify => GetMember<StringType>("Prettify");
 
         /// <summary>
         /// Implicit conversion to double
