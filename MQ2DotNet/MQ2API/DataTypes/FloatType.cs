4e291f6855458946e868636f75ae50272f961683
Anthony Spruyt <aspruyt@hotmail.co.uk>
2023-06-25 23:23:36 +1000

    Update FloatType to latest spec

--- MQ2DotNet/MQ2API/DataTypes/FloatType.cs
+++ MQ2DotNet/MQ2API/DataTypes/FloatType.cs
@@ -3,7 +3,8 @@
 namespace MQ2DotNet.MQ2API.DataTypes
 {
     /// <summary>
-    /// MQ2 type for a single precision float
+    /// MQ2 type for a single precision float.
+    /// Last Verified: 2023-06-25
     /// </summary>
     [PublicAPI]
     [MQ2Type("float")]
@@ -12,6 +13,41 @@ namespace MQ2DotNet.MQ2API.DataTypes
         internal FloatType(MQ2TypeFactory mq2TypeFactory, MQ2TypeVar typeVar) : base(mq2TypeFactory, typeVar)
         {
         }
+
+        /// <summary>
+        /// TODO: new member
+        /// </summary>
+        public string Deci => GetMember<StringType>("Deci");
+
+        /// <summary>
+        /// TODO: new member
+        /// </summary>
+        public string Centi => GetMember<StringType>("Centi");
+
+        /// <summary>
+        /// TODO: new member
+        /// </summary>
+        public string Milli => GetMember<StringType>("Milli");
+
+        /// <summary>
+        /// TODO: new member
+        /// </summary>
+        public int? Int => GetMember<IntType>("Int");
+
+        /// <summary>
+        /// TODO: new member
+        /// </summary>
+        public string Precision => GetMember<StringType>("Precision");
+
+        /// <summary>
+        /// TODO: new member
+        /// </summary>
+        public uint? Raw => GetMember<IntType>("Raw");
+
+        /// <summary>
+        /// TODO: new member
+        /// </summary>
+        public string Prettify => GetMember<StringType>("Prettify");
 
         /// <summary>
         /// Implicit conversion to nullable float
