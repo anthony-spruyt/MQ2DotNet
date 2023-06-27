bf6cabd6bf173055299523cfa2d845a9878d1481
Anthony Spruyt <aspruyt@hotmail.co.uk>
2023-06-28 00:13:48 +1000

    Update XTargetType to latest spec.

--- MQ2DotNet/MQ2API/DataTypes/XTargetType.cs
+++ MQ2DotNet/MQ2API/DataTypes/XTargetType.cs
@@ -4,11 +4,12 @@ using MQ2DotNet.Services;
 namespace MQ2DotNet.MQ2API.DataTypes
 {
     /// <summary>
-    /// MQ2 type for an entry in the xtarget list
+    /// MQ2 type for an entry in the xtarget list.
+    /// Last Verified: 2023-06-28
     /// </summary>
     [PublicAPI]
     [MQ2Type("xtarget")]
-    public class XTargetType : MQ2DataType // MQ2XTargetType inherits MQ2SpawnType, but we can't do this as it needs to do a transform on the VarPtr before calling the base's GetMember
+    public class XTargetType : MQ2DataType
     {
         internal XTargetType(MQ2TypeFactory mq2TypeFactory, MQ2TypeVar typeVar) : base(mq2TypeFactory, typeVar)
         {
@@ -18,22 +19,22 @@ namespace MQ2DotNet.MQ2API.DataTypes
         /// Extended target type e.g. Auto Hater
         /// </summary>
         public string TargetType => GetMember<StringType>("TargetType");
-
+        
         /// <summary>
         /// Spawn ID
         /// </summary>
-        public int? ID => GetMember<IntType>("ID");
-
+        public uint? ID => GetMember<IntType>("ID");
+        
         /// <summary>
         /// Spawn's name
         /// </summary>
         public string Name => GetMember<StringType>("Name");
-
+        
         /// <summary>
         /// Your percentage aggro on the spawn
         /// </summary>
-        public int? PctAggro => GetMember<IntType>("PctAggro");
-
+        public uint? PctAggro => GetMember<IntType>("PctAggro");
+        
         /// <summary>
         /// Spawn in the XTarget slot
         /// </summary>
