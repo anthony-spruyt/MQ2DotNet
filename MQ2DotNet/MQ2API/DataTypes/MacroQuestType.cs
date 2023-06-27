90e07f08637886703cb17db466eb88953764bde1
Anthony Spruyt <aspruyt@hotmail.co.uk>
2023-06-27 20:14:39 +1000

    Update MacroQuestType to latest spec

--- MQ2DotNet/MQ2API/DataTypes/MacroQuestType.cs
+++ MQ2DotNet/MQ2API/DataTypes/MacroQuestType.cs
@@ -3,18 +3,21 @@
 namespace MQ2DotNet.MQ2API.DataTypes
 {
     /// <summary>
-    /// MQ2 type for general information about MQ2
+    /// MQ2 type for general information about MQ2.
+    /// Last Verified: 2023-06-27
     /// </summary>
     [PublicAPI]
     [MQ2Type("macroquest")]
-    public class MacroQuestType : EverQuestType
+    public class MacroQuestType : MQ2DataType
     {
         internal MacroQuestType(MQ2TypeFactory mq2TypeFactory, MQ2TypeVar typeVar) : base(mq2TypeFactory, typeVar)
         {
+            Path = new IndexedStringMember<string>(this, "Path");
         }
 
         /// <summary>
-        /// Last normal error message
+        /// Last normal error message.
+        /// Comment in native MQ client: // QUIT SETTING THIS MANUALLY, USE MacroError, FatalError, ETC
         /// </summary>
         public string Error => GetMember<StringType>("Error");
 
@@ -36,16 +39,46 @@ namespace MQ2DotNet.MQ2API.DataTypes
         /// <summary>
         /// Build number
         /// </summary>
-        public int? Build => GetMember<IntType>("Build");
+        public uint? Build => GetMember<IntType>("Build");
+
+        /// <summary>
+        /// TODO: new member
+        /// </summary>
+        public string BuildName => GetMember<StringType>("BuildName");
+
+        /// <summary>
+        /// Gets the path by string index.
+        /// 
+        /// Index values:
+        /// - root
+        /// - config
+        /// - crashdumps
+        /// - logs
+        /// - mqini
+        /// - macros
+        /// - plugins
+        /// - resources
+        /// </summary>
+        public IndexedStringMember<string> Path { get; }
 
         /// <summary>
         /// Version number of MQ2Main.dll
         /// </summary>
         public string Version => GetMember<StringType>("Version");
-
+        
         /// <summary>
         /// Internal name of build e.g. RedGuides
         /// </summary>
         public string InternalName => GetMember<StringType>("InternalName");
+
+        /// <summary>
+        /// Parser version.
+        /// </summary>
+        public uint? Parser => GetMember<IntType>("Parser");
+
+        /// <summary>
+        /// Is anonymized?
+        /// </summary>
+        public bool Anonymize => GetMember<BoolType>("Anonymize");
     }
 }
