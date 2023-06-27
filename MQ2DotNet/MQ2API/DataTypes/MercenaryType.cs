08e87d0dc2b26efc896d83a7053cc11892c03bfd
Anthony Spruyt <aspruyt@hotmail.co.uk>
2023-06-27 20:45:01 +1000

    Update MercenaryType to latest spec

--- MQ2DotNet/MQ2API/DataTypes/MercenaryType.cs
+++ MQ2DotNet/MQ2API/DataTypes/MercenaryType.cs
@@ -3,7 +3,8 @@
 namespace MQ2DotNet.MQ2API.DataTypes
 {
     /// <summary>
-    /// MQ2 type for a mercenary
+    /// MQ2 type for a mercenary.
+    /// Last Verified: 2023-06-27
     /// </summary>
     [PublicAPI]
     [MQ2Type("mercenary")]
@@ -16,21 +17,32 @@ namespace MQ2DotNet.MQ2API.DataTypes
         /// <summary>
         /// Number of unspent mercenary AA points
         /// </summary>
-        public int? AAPoints => GetMember<IntType>("AAPoints");
-
+        public uint? AAPoints => GetMember<IntType>("AAPoints");
+        
         /// <summary>
         /// Current stance of the mercenary e.g. Balanced
+        /// TODO: map to an enum
         /// </summary>
         public string Stance => GetMember<StringType>("Stance");
 
         /// <summary>
+        /// Current State of the mercenary
+        /// </summary>
+        public string State => GetMember<StringType>("State");
+
+        /// <summary>
         /// Current state ID of the mercenary as a number.
         /// </summary>
-        public int? StateID => GetMember<IntType>("StateID");
-
+        public uint? StateID => GetMember<IntType>("StateID");
+        
         /// <summary>
         /// Index of the mercenary in your mercenary list (1 based)
         /// </summary>
-        public int? Index => GetMember<IntType>("Index");
+        public uint? Index => GetMember<IntType>("Index");
+
+        /// <summary>
+        /// Name of the mercenary
+        /// </summary>
+        public string Name => GetMember<StringType>("Name");
     }
 }
